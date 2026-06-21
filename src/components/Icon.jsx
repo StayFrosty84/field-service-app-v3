a1275e4ee1c52d4538a17ec6ddfde72115b71426
Alex Bitar <alexander.a.bitar@gmail.com>
2026-06-21 01:22:15 -0400

    Icon: add square, circle, arrow-up-right, trash-2

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -154,6 +154,20 @@ const ICONS = {
       <path d="M5 3v4M3 5h4M19 17v4M17 19h4" />
     </>
   ),
+  square: <rect width="18" height="18" x="3" y="3" rx="2" />,
+  circle: <circle cx="12" cy="12" r="10" />,
+  'arrow-up-right': (
+    <>
+      <path d="M7 17 17 7" />
+      <path d="M7 7h10v10" />
+    </>
+  ),
+  'trash-2': (
+    <>
+      <path d="M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+      <path d="M10 11v6M14 11v6" />
+    </>
+  ),
 };
 
 export default function Icon({ name, size = 18, className = '', ...rest }) {
